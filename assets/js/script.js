d3f1f5968eb11f8d8faf0e483c2b0913c699475b
irshath96 <irshathmohamed96@gmail.com>
2023-08-27 00:21:53 +0530

    commit
    code updated

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+$('.logo-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll:1,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplayspeed: 2000,
+    infinite: true,
+});
